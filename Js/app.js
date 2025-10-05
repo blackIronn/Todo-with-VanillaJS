f84f1a87ef53192dc47f084477b7d6c5d15ec529
Burak Karademir <94866565+blackIronn@users.noreply.github.com>
2025-10-05 21:41:14 +0300

    genel işlev tamamlandı

--- Js/app.js
+++ Js/app.js
@@ -22,12 +22,18 @@ const showTodo = () => {
     const todoArea = document.getElementById("todo-cards");
     todoArea.innerHTML = todoList
         .map(todo => `<div class= "card">${todo}
-            <button type="button" class="change-status" onclick="changeStatus()">...</button>
+            <button type="button" class="change-status" onclick="addInProgress('${todo}')">...</button>
             </div>`)
         .join('');
 
-    const todoCount = document.getElementById("todo-count");
-    todoCount.innerHTML = todoList.length;
+        const todoCount = document.getElementById("todo-count");
+    if (todoList.length > 0){
+        
+        todoCount.innerHTML = todoList.length;
+    }
+    else{
+        todoCount.innerHTML = "0";
+    }
 }
 
 const showInProgress = () => {
@@ -35,12 +41,17 @@ const showInProgress = () => {
     const inProgressArea = document.getElementById("inprogress-cards");
     inProgressArea.innerHTML = inProgressList
         .map(inProgress => `<div class="card">${inProgress}
-            <button type="button" class="change-status" onclick="changeStatus()">...</button>
+            <button type="button" class="change-status" onclick="addClosed('${inProgress}')">...</button>
             </div>`)
         .join('');
 
-    const inProgressCount = document.getElementById("inprogress-count");
-    inProgressCount.innerHTML = inProgressList.length;
+        const inProgressCount = document.getElementById("inprogress-count");
+    if (inProgressList.length > 0){
+        inProgressCount.innerHTML = inProgressList.length;
+    }
+    else{
+        inProgressCount.innerHTML = "0";
+    }
 }
 
 const showClosed = () => {
@@ -48,12 +59,17 @@ const showClosed = () => {
     const closedArea = document.getElementById("closed-cards");
     closedArea.innerHTML = closedList
         .map(closed => `<div class="card">${closed}
-            <button type="button" class="change-status" onclick="changeStatus()">...</button>
+            <button type="button" class="change-status" onclick="removeTodo('${closed}')">...</button>
             </div>`)
         .join('');
 
-    const closedCount = document.getElementById("closed-count");
-    closedCount.innerHTML = closedList.length;
+        const closedCount = document.getElementById("closed-count");
+    if (closedList.length > 0){
+        closedCount.innerHTML = closedList.length;
+    }
+    else{
+        closedCount.innerHTML = "0";
+    }
 }
 
 const resetInput = () => {
@@ -62,10 +78,30 @@ const resetInput = () => {
 
 const updateStatus = () => {
     showTodo();
-    //showInProgress();
-    //showClosed();
+    showInProgress();
+    showClosed();
 
     resetInput();
 }
 
+
+const addInProgress = (item) => {
+    todoList = todoList.filter(todo =>todo !== item);
+    inProgressList.push(item);
+
+    updateStatus();
+}
+
+const addClosed = (item) => {
+    inProgressList = inProgressList.filter(todo => todo !== item);
+    closedList.push(item);
+
+    updateStatus();
+}
+
+const removeTodo = (item) => {
+    closedList = closedList.filter(todo => todo !== item);
+
+    updateStatus(); 
+}
 
