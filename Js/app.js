aef7a3bdfa75ee84db70eb52e2575596a43faca0
Burak Karademir <94866565+blackIronn@users.noreply.github.com>
2025-10-05 18:13:35 +0300

    show todo

--- Js/app.js
+++ Js/app.js
@@ -13,6 +13,17 @@ const getTodoName = () =>{
 const createTodo = () => {
     let todoName = getTodoName();
     todoList.push(todoName);
+
+    showTodo();
+}
+
+const showTodo = () => {
+
+    const todoArea = document.getElementById("todo-cards");
+    todoArea.innerHTML = todoList.map(todo => `<div class= "card">${todo}</div>`).join('');
+
+    const todoCount = document.getElementById("todo-count");
+    todoCount.innerHTML = todoList.length;
 }
 
 
